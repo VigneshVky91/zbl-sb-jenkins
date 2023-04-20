e47b3762eeec4dbf1e36f6f7e2bf5984d43ea8fa
Vignesh <Vignesh@telebright.com>
2023-04-20 20:48:01 +0530

    first commit

---
+++ src/main/java/com/example/demo/ZblSbJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZblSbJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZblSbJenkinsApplication.class, args);
+	}
+
+}
